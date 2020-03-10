6c4483e92826301b1bf9b91aa21bc15b3c40ee06
jonathanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-03-10 20:49:18 -0300

    terminar

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const superagent = require('superagent');
-
+const fs = require('fs');
 function imprimirMuseos(error, respuesta) {
   if (error) {
     throw new Error('algo se rompió', error);
@@ -17,6 +17,22 @@ console.log('Antes de llamar a superagent')
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
-  .end(imprimirMuseos)
+  .end(
+    const nombres=  
+    fs.writeFile('./museos.txt', "hola", procesarArchivo)
+  )
+
 
 console.log('Después de llamar a superagent')
+
+
+function procesarArchivo(error, res) {
+
+  if (error !== null) {
+    console.error('Algo salió mal :(')
+    console.error(error)
+  } 
+}
+
+// El segundo parámetro es necesario para que NodeJS sepa que queremos leerlo como String
+// Si no lo pasamos devuelve un Buffer (una chorrera de bytes).
