40f584256fd4c1696fb976caba7a9aaeb516820f
jonathanfreiredasilva <jonathan.agustin.fds@gmail.com>
2020-03-12 21:32:07 -0300

    terminar

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 const superagent = require('superagent');
 const fs = require('fs');
-var nombres = []
-
-function imprimirMuseos(error, respuesta) {
-  if (error) {
-    throw new Error('algo se rompió', error);
-  }
-
-  const cantidad = respuesta.body.count;
-  const museos = respuesta.body.results;
-
-  console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
-}
 
 console.log('Antes de llamar a superagent')
 
+
+const getMuseos=()=>{
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
   .end(
-        procesarArchivo    // 
-
+        procesarArchivo    
   )
+}
 
+const getOrga=()=>{
+  superagent
+  .get('https://www.cultura.gob.ar/api/v2.0/organismos')
+  .query({ format: 'json' })
+  .end( procesarArchivoOrg )
+  }
 
 console.log('Después de llamar a superagent')
 
-
 function procesarArchivo(error, res) {
-
   if (error !== null) {
     console.error('Algo salió mal :(')
     console.error(error)
   }   
-
-  const museos = res.body.results;
+  const museos = res.body.results
   const nombres = museos.map((element) => 
     element.nombre +"( " + element.direccion +" )" + ", por cualquier consulta comunicarse al : "+element.telefono
   );
-  
- fs.writeFile('./museos.txt', nombres.join('\n'), procesarArchivo) 
-
+       fs.writeFile('./MUSEOS.txt', nombres.join('\n'), (e)=>{}) 
 }
 
-// El segundo parámetro es necesario para que NodeJS sepa que queremos leerlo como String
-// Si no lo pasamos devuelve un Buffer (una chorrera de bytes).
+
+function procesarArchivoOrg(error, res) {
+  if (error !== null) {
+    console.error('Algo salió mal :(')
+    console.error(error)
+  }   
+  const org = res.body.results;
+  const nombres = org.map((element) => 
+    element.nombre +"( " + element.direccion +" )" + ", por cualquier consulta comunicarse al : "+element.telefono
+  );
+ fs.appendFile('./MUSEOS.txt', nombres.join('\n'), (e)=>{}) 
+}
+
+
+getMuseos()
+getOrga()
